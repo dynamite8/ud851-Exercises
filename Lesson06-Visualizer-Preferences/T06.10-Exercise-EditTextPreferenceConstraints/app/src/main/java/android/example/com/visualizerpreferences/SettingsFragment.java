1a33bd42441b5037febb332f091303f62ead8c97
Jenny Chang Ho <Jchangho@grubhub.com>
2018-02-02 15:34:20 -0500

    Completed Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,9 +27,9 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
-// TODO (1) Implement OnPreferenceChangeListener
+// COMPLETED (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -49,9 +49,14 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             if (!(p instanceof CheckBoxPreference)) {
                 String value = sharedPreferences.getString(p.getKey(), "");
                 setPreferenceSummary(p, value);
+
+                // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+                if (p instanceof EditTextPreference) {
+                    EditTextPreference editTextPreference = (EditTextPreference) p;
+                    p.setOnPreferenceChangeListener(this);
+                }
             }
         }
-        // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
     }
 
     @Override
@@ -88,10 +93,29 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         }
     }
 
-    // TODO (2) Override onPreferenceChange. This method should try to convert the new preference value
+    // COMPLETED (2) Override onPreferenceChange. This method should try to convert the new preference value
     // to a float; if it cannot, show a helpful error message and return false. If it can be converted
     // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
     // an error message and return false. If it is a valid number, return true.
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast error = Toast.makeText(getContext(), "Invalid scale factor entered.  Try a number between 0 and 3.", Toast.LENGTH_LONG);
+
+        if (preference instanceof EditTextPreference && newValue instanceof String) {
+            try {
+                float value = Float.valueOf((String)newValue);
+                boolean isValid = value >=0 && value <= 3;
+                if (!isValid) {
+                    error.show();
+                }
+                return isValid;
+            } catch (Exception e) {
+                // Do nothing
+                error.show();
+            }
+        }
+        return false;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
