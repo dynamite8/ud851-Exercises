9db104f97e263b1d96d7a8628a9e9a819f344476
Jenny Chang Ho <orthographic@gmail.com>
2018-02-17 17:09:42 -0500

    Completed T09.06-Exercise-Delete

--- Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -156,14 +156,33 @@ public class TaskContentProvider extends ContentProvider {
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
-        // TODO (1) Get access to the database and write URI matching code to recognize a single item
+        // COMPLETED (1) Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
 
-        // TODO (2) Write the code to delete a single row of data
-        // [Hint] Use selections to delete an item by its row ID
+        int numberOfRowsDeleted = 0;
 
-        // TODO (3) Notify the resolver of a change and return the number of items deleted
+        switch(match) {
+            case TASK_WITH_ID:
+                // COMPLETED (2) Write the code to delete a single row of data
+                // [Hint] Use selections to delete an item by its row ID
+                String id = uri.getPathSegments().get(1);
+                String mSelection = "_id=?";
+                String[] mSelectionArgs = new String[]{id};
 
-        throw new UnsupportedOperationException("Not yet implemented");
+                numberOfRowsDeleted = db.delete(TABLE_NAME, mSelection, mSelectionArgs);
+
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // COMPLETED (3) Notify the resolver of a change and return the number of items deleted
+        if (numberOfRowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        
+        return numberOfRowsDeleted;
     }
 
 
